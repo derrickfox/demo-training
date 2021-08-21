013099524bca39eff0a756ba6b7ad6588c4375db
Me <fox.derrick@gmail.com>
2021-08-21 19:03:43 -0400

    added basic components

--- src/app/training/training.component.ts
+++ src/app/training/training.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TrainingComponent implements OnInit {
 
+  public ongoingTraining = false;
+
   constructor() { }
 
   ngOnInit() {
