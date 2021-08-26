1b50d61dff731f4e62fc062e1563b0a3112413c1
Me <fox.derrick@gmail.com>
2021-08-26 17:07:52 -0400

    saving

--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -10,6 +10,7 @@ export class TrainingService {
         { id: 'burpees', name: 'Burpees', duration: 4, calories: 8 }
     ];
     private runningExercise: Exercise;
+    private exercises: Exercise[] = [];
 
     public getAvailableExercises(): Exercise[] {
         return this.availableExercises.slice();
@@ -20,6 +21,28 @@ export class TrainingService {
         this.runningExercise = this.availableExercises.find(ex => ex.id === selectedId);
         console.log('this.runningExercise', this.runningExercise)
         this.exerciseChanged.next({ ...this.runningExercise })
+    }
+
+    public completeExercise(): void {
+        this.exercises.push({
+            ...this.runningExercise,
+            date: new Date,
+            state: 'completed'
+        });
+        this.runningExercise = null;
+        this.exerciseChanged.next(null);
+    }
+
+    public cancelExercise(progress: number): void {
+        this.exercises.push({
+            ...this.runningExercise,
+            duration: this.runningExercise.duration * (progress / 100),
+            calories: this.runningExercise.duration * (progress / 100),
+            date: new Date,
+            state: 'cancelled'
+        });
+        this.runningExercise = null;
+        this.exerciseChanged.next(null);
     }
 
     public getRunningExercise() {
