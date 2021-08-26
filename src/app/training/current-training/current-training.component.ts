4d744f9ba1f54c59c0ec6c8b82901248634842ad
Me <fox.derrick@gmail.com>
2021-08-26 13:27:06 -0400

    fixed bugs

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { MatDialog } from '@angular/material';
+import { TrainingService } from '../training.service';
 import { StopTrainingComponent } from './stop-training.component';
 
 @Component({
@@ -10,21 +11,22 @@ import { StopTrainingComponent } from './stop-training.component';
 export class CurrentTrainingComponent implements OnInit {
   @Output() trainingExit = new EventEmitter();
   public progress: number = 0;
-  public timer: number;
+  public timer: any;
 
-  constructor(private dialog: MatDialog) { }
+  constructor(private dialog: MatDialog, private trainingService: TrainingService) { }
 
   ngOnInit() {
     this.startOrResumeTimer();
   }
 
   public startOrResumeTimer(): void {
+    const step = this.trainingService.getRunningExercise().duration / 100 * 10000;
     this.timer = setInterval(() => {
-      this.progress = this.progress + 5;
+      this.progress = this.progress + 1;
       if (this.progress >= 100) {
         clearInterval(this.timer);
       }
-    }, 1000)
+    }, step)
   }
 
   public onStop(): void {
--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -4,21 +4,25 @@ import { Exercise } from "./exercise.model";
 export class TrainingService {
     public exerciseChanged = new Subject<Exercise>();
     private availableExercises: Exercise[] = [
-        { id: 'crunches', name: 'Crunches', duration: 44, calories: 3 },
-        { id: 'touch-toes', name: 'Touch Toes', duration: 180, calories: 12 },
-        { id: 'side-lunges', name: 'Side Lunges', duration: 120, calories: 3 },
-        { id: 'burpees', name: 'Burpees', duration: 60, calories: 8 }
+        { id: 'crunches', name: 'Crunches', duration: 1, calories: 3 },
+        { id: 'touch-toes', name: 'Touch Toes', duration: 2, calories: 12 },
+        { id: 'side-lunges', name: 'Side Lunges', duration: 3, calories: 3 },
+        { id: 'burpees', name: 'Burpees', duration: 4, calories: 8 }
     ];
-
     private runningExercise: Exercise;
-
 
     public getAvailableExercises(): Exercise[] {
         return this.availableExercises.slice();
     }
 
     public startExercise(selectedId: string) {
+        console.log('start exercise!!')
         this.runningExercise = this.availableExercises.find(ex => ex.id === selectedId);
+        console.log('this.runningExercise', this.runningExercise)
         this.exerciseChanged.next({ ...this.runningExercise })
+    }
+
+    public getRunningExercise() {
+        return { ...this.runningExercise };
     }
 }
