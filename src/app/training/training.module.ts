46eba6773323072232658a7406d0efa03871126e
Derrick Fox <fox.derrick@gmail.com>
2022-03-12 15:44:44 -0500

    split up training routes and lazy loading

--- src/app/training/training.module.ts
+++ src/app/training/training.module.ts
@@ -1,14 +1,10 @@
-import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
-import { FlexLayoutModule } from "@angular/flex-layout";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { AngularFirestoreModule } from "angularfire2/firestore";
-import { MaterialModule } from "../material.module";
 import { SharedModule } from "../shared/shared.module";
 import { CurrentTrainingComponent } from "./current-training/current-training.component";
 import { StopTrainingComponent } from "./current-training/stop-training.component";
 import { NewTrainingComponent } from "./new-training/new-training.component";
 import { PastTrainingsComponent } from "./past-trainings/past-trainings.component";
+import { TrainingRoutingModule } from "./training-routing.module";
 import { TrainingComponent } from "./training.component";
 
 @NgModule({
@@ -20,9 +16,8 @@ import { TrainingComponent } from "./training.component";
         StopTrainingComponent
     ],
     imports: [
-        ReactiveFormsModule,
-        AngularFirestoreModule,
-        SharedModule
+        SharedModule,
+        TrainingRoutingModule
     ],
     exports: [],
     entryComponents: [StopTrainingComponent]
